b88e3c1eea6e21216d21882a04a102740a1b0b04
abelmuro <abelmuro93@gmail.com>
2023-11-18 14:09:15 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import App from './App.js';
+import * as serviceWorker from './ServiceWorkers/ServicesWorkers.js';
 import ReactDOM from 'react-dom/client';
 
 const rootRef = document.getElementById('root');
 const root = ReactDOM.createRoot(rootRef);
 root.render(<App/>);
+
+serviceWorker.register();
