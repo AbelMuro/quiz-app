c5050580935e5defeffe15636621400907868494
abelmuro <abelmuro93@gmail.com>
2023-11-18 14:58:32 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './App.js';
-import * as serviceWorker from '../ServiceWorkers.js';
+import * as serviceWorker from './ServiceWorkers.js';
 import ReactDOM from 'react-dom/client';
 
 const rootRef = document.getElementById('root');
