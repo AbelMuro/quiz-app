c65504526b08e79c42d3bf42a32baf3e93ddbc91
abelmuro <abelmuro93@gmail.com>
2023-11-18 16:38:11 -0800

    commit

--- src/ServiceWorker.js
+++ src/ServiceWorker.js
@@ -1,49 +1,15 @@
-var CACHE_NAME = 'Quiz App';
-var urlsToCache = [
-  '/',
-  '/completed'
-];
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
 
-
-// Install service worker
-self.addEventListener('install', event => {
-  // Perform the install steps
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Cache opened');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
- 
-// Cache and return the requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Return response as Cache is hit 
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
- 
-// Update service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['Quiz App'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/style.css",
+      "/app.js"
+    ]),
   );
 });
