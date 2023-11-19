2a89032eccfc5f9fa68c2cedb35de0dd4efaba84
abelmuro <abelmuro93@gmail.com>
2023-11-18 16:36:02 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import App from './App.js';
-import * as serviceWorker from './ServiceWorker.js';
 import ReactDOM from 'react-dom/client';
 
 const rootRef = document.getElementById('root');
@@ -26,5 +25,5 @@ const registerServiceWorker = async () => {
       }
     }
   };
-  
+
   registerServiceWorker()
