148c0de25eb190ade485986815b614d0771d925c
abelmuro <abelmuro93@gmail.com>
2023-11-18 17:30:27 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -5,3 +5,16 @@ import ReactDOM from 'react-dom/client';
 const rootRef = document.getElementById('root');
 const root = ReactDOM.createRoot(rootRef);
 root.render(<App/>);
+
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register(`/ServiceWorker.js`).then((registration) => {
+            console.log('Worker registration is successful', registration.scope);
+        }, (err) => {
+            console.log('Worker registration has failed', err);
+        })
+    });
+} else {
+    console.log('Service Worker is not supported by your browser.');
+}
