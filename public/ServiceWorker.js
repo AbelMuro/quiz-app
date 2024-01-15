69f241fde2bb02c615094598c25ca151d49465ec
abelmuro <abelmuro93@gmail.com>
2024-01-14 16:50:08 -0800

    commit

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -1,56 +1,9 @@
-var CACHE_NAME = 'Quiz App';
-var urlsToCache = [
-  '/'
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
+// this function will let the browser make a request to a server for images to be displayed
+// if the server is unavailable, then the browser will access the cache for the images
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'image',
+    new workbox.strategies.NetworkFirst()
+);
 
-// Install service worker
-self.addEventListener('install', e => {
-  // Perform the install steps
-  e.waitUntil(                
-    caches.open(CACHE_NAME)         
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
- 
-// Cache and return the requests
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(function(response) {
-          if (response) 
-            return response;
-          else
-            return fetch(e.request)
-                .then((response) => {
-                    const cloneResponse = response.clone();
-                    caches.open('Quiz App')
-                      .then((cache) => {
-                          cache.put(e.request, cloneResponse)
-                      })
-                  return networkResponse;
-                })
-        })
-        .catch(() => {
-          return new Response('Error');
-        })
-  );
-});
- 
-// Update service worker
-self.addEventListener('activate', e => {
-  var cacheWhitelist = ['Quiz App'];        
-  e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
